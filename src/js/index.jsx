5f7898ff08019ccf9433c6923ad90084b03cbd73
pasha <pasha.nitw@gmail.com>
2015-03-05 18:39:35 +0530

    haandle bar pending

--- src/js/index.jsx
+++ src/js/index.jsx
@@ -1,5 +1,7 @@
 var React = require('react'),
-    App = require('./components/App.jsx');
-console.log($);
+    App = require('./components/App.jsx'),
+    JST=require('JST');
+console.log("jst is",JST);
+
 
 React.render(<App />, document.getElementById('main'));
